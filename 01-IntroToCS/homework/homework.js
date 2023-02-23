2b47bae19b8f102ac60e811df1e4322b54bdcab4
Hernan Mongelos <107436230+hamf87@users.noreply.github.com>
2023-02-22 23:22:36 -0300

    1.2

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -9,7 +9,17 @@ function BinarioADecimal(num) {
    return res;
 }
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let bin =[];
+
+   if (num === 0) return "0";
+
+   while( num >= 1){
+      bin.unshift(num % 2);
+      num = Math.trunc(num / 2);
+   }
+   return bin.join("");
+}
 
 module.exports = {
    BinarioADecimal,
