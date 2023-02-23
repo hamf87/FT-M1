ed2009e89e9e09c04514962b2cb50559843d58b2
Hernan Mongelos <107436230+hamf87@users.noreply.github.com>
2023-02-22 23:37:36 -0300

    1.2

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,12 +1,7 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   let res = "";
-   while (num>0){
-      res = (num % 2) + res;
-      num = ~~(num / 2);
-   }
-   return res;
+
 }
 
 function DecimalABinario(num) {
