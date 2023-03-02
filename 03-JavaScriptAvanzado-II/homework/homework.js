96eb6ea591c64bfb1d4fca3d28c3fec7ea9f949b
Hernan Mongelos <107436230+hamf87@users.noreply.github.com>
2023-03-02 17:22:14 -0300

    Hernan 1.4

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -80,8 +80,8 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-getNombreInstructor = getNombre.bind(instructor);
-getNombreAlumno = getNombre.bind(alumno);
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
