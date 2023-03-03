9f08872977e45888d06d3d2c7d75df41f13f2796
Hernan Mongelos <107436230+hamf87@users.noreply.github.com>
2023-03-02 23:08:29 -0300

    Hernan 1.1

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -14,7 +14,11 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if(n === 0 || n === 1) return 1;
+  if(n < 0) return 'Numeros factoriales no puede ser negativo'
+  return n* nFactorial(n - 1)
+}
 
 function nFibonacci(n) {}
 
