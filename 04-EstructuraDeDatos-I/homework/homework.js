9359f2cc19eaf7de8be1a098b5a11dfff895ec52
Hernan Mongelos <107436230+hamf87@users.noreply.github.com>
2023-03-06 23:15:31 -0300

    Hernan 1.2

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -37,7 +37,22 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.array = [];
+}
+
+Queue.prototype.enqueue = function (elemento) {
+  return this.array.push(elemento);
+}
+
+Queue.prototype.dequeue = function () {
+  return this.array.shift();
+}
+
+Queue.prototype.size = function () {
+  return this.array.length;
+}
+
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
